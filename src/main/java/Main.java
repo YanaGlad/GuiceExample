90fc01debf173f5e5efc61c0e8ff45463ff51381
YanaGlad <olesya_leader@mail.ru>
2020-10-03 17:21:47 +0300

    Update 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import com.google.inject.Injector;
 
 import java.io.IOException;
 
+//Created by Yana Gladkikh Telegram @YanaGlad12
 public class Main {
     public static void main(String[] args) throws IOException {
         final Injector injector = Guice.createInjector(new MainModule());
